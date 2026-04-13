4dee4d74bfabe1756cc284a7d946e9e5d05ae142
fercscomercio <fercs.comercio@gmail.com>
2026-04-13 18:58:48 +0200

    Update build.py

--- build.py
+++ build.py
@@ -2,19 +2,13 @@ from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
 use_plugin("python.unittest")
-use_plugin("python.flake8")     # Análisis estático de estilo [cite: 165]
-use_plugin("python.coverage")   # Auditoría de ejecución [cite: 166]
-use_plugin("python.distutils")
+use_plugin("python.install_dependencies")
 
-name = "BioGuard_Inventory"
+name = "bioguard_inventory"
 default_task = "publish"
 
 @init
-def set_properties(project):
-    # Umbral de rechazo: El build fallará si la cobertura es < 100% [cite: 98, 171]
-    project.set_property("coverage_break_build", True)
-    project.set_property("coverage_threshold_warn", 100)
-    
-    # Política de Estilo: El build fallará ante errores de formato PEP8 [cite: 172]
-    project.set_property("flake8_break_build", True)
-    project.set_property("flake8_verbose_output", True)
+def initialize(project):
+    project.build_depends_on('flask')
+    project.set_property('dir_source_main_python', 'src/main/python')
+    project.set_property('dir_source_unittest_python', 'src/unittest/python')
